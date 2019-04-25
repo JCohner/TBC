7654090702b230cdf924737cca2f66938189c5c6
JCohner <joshuacohen2019@u.northwestern.edu>
2019-04-24 19:09:23 -0500

    more statefull serial data passing

--- serial_com/matlab_to_tiva.py
+++ serial_com/matlab_to_tiva.py
@@ -1,9 +1,12 @@
+import pdb
+
+
 #Seral Initialization
 import serial
 ser = serial.Serial()
 ser.baudrate = 115200
-ser.port = 'COM22'
-#ser.open()
+ser.port = 'COM15'
+ser.open()
 
 #MATLAB Initialization
 import matlab.engine
@@ -14,13 +17,26 @@ print("matlab found w name: " + str(name[0]))
 XPosValue = eng.workspace['XPosValue']
 testStatus = eng.workspace['selectedButton']
 
-print(XPosValue)
+sent_messages = []
+
 print(testStatus)
+message = 0 #make sure message is defined before its called
+
+#pdb.set_trace()
 
 while(testStatus != "Cancel Test"):
 	testStatus = eng.workspace['selectedButton']
-	if (XPosValue != eng.workspace['XPosValue']):
+	#print(testStatus)
+	if ((XPosValue != eng.workspace['XPosValue']) & (testStatus == "Begin Test")):
 		XPosValue = eng.workspace['XPosValue']
-		print("XPosValue changed to: " + str(XPosValue))
-		message = XPosValue
-		#ser.write(message.encode())
+		message = str(XPosValue)[:7]
+		sent_messages.append(message)
+		print("XPosValue changed to: " + message)
+
+		ser.write(message.encode())
+
+	if ((testStatus == "Prepare Test") & (len(sent_messages) != 0)):
+		#pdb.set_trace()
+		sent_messages.pop(0)
+		received = ser.read(len(message.encode()))
+		print('Message received: ' + received.decode())
