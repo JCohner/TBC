2e1e0252c0cff56c362696a74f648efc0954785f
JCohner <joshuacohen2019@u.northwestern.edu>
2019-04-30 20:35:28 -0500

    hello world example of matlab -> python connection

--- serial_com/hello_world_data_passer.m
+++ serial_com/hello_world_data_passer.m
@@ -1,3 +1,4 @@
+% HELLO WORLD AND HOW TO DOC FOR OPENING MATLAB PYTHON CONNECTION
 %Matlab Engine - Matlab side functions
 % matlab.engine.shareEngine('sharedName') %shared name is optional
 % matlab.engine.engineName
